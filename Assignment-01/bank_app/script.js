194eb17a6716794ebe7a5f79a4aea2e27a37a2f0
Nahid Hassan <mdnahidhassan01@gmail.com>
2024-04-09 19:52:48 +0600

    floating amount handled with precison

--- Assignment-01/bank_app/script.js
+++ Assignment-01/bank_app/script.js
@@ -7,12 +7,15 @@ class BankAccount {
 
   deposit(amount) {
     this.balance += amount;
+    this.balance = this.balance.toFixed(2);
     console.log(`$${amount} deposit successfull!`);
   }
 
   withdraw(amount) {
+    amount = parseFloat(amount.toFixed(2));
     if (this.balance >= amount) {
       this.balance -= amount;
+      this.balance = this.balance.toFixed(2);
       console.log(
         `$${amount} withdrawn successfull! Your new balance is $${this.balance}`
       );
@@ -35,6 +38,7 @@ class BankAccount {
 let person1 = new BankAccount(1001, "John Doe", 100);
 let person2 = new BankAccount(1002, "Kim John", 200);
 
+console.log("First Person\n------------");
 person1.getBalance();
 person1.deposit(30);
 person1.withdraw(300);
@@ -42,11 +46,12 @@ person1.withdraw(80);
 person1.getBalance();
 person1.displayAccountInfo();
 
-console.log("\n\n");
+console.log("\n");
 
+console.log("Second Person\n------------");
 person2.getBalance();
 person2.deposit(50);
 person2.withdraw(300);
-person2.withdraw(80);
+person2.withdraw(80.5);
 person2.getBalance();
 person2.displayAccountInfo();
